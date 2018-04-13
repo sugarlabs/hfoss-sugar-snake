5eb4a6b580e3548212cb8425c56e3d45d7423930
axk4545 <abkahrs@gmail.com>
2018-04-13 09:35:43 -0400

    redo architecture to match what it should be

--- mainPlaked.py
+++ mainPlaked.py
@@ -25,6 +25,53 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+
+"""
+class snakeActivity(activity.Activity):
+
+"""
+
+
+# Sugar Imports
+from sugar3.activity.activity import Activity
+from sugar3.activity.widgets import StopButton
+from sugar3.activity.widgets import ActivityButton
+
+
+# Gtk Import
+from gi.repository import Gtk
+from gettext import gettext as _
+
+
+class snakeActivity(activity.Activity):
+    def __init__(self, sugar_handle):
+        Activity.__init__(self, sugar_handle)
+
+        # Create a Toolbar
+        toolbar = Gtk.Toolbar()
+
+        # Add toolbar to Sugar Activity Toolbar Space
+        self.set_toolbar_box(toolbar)
+
+        # Add Activity Button
+        toolbar.insert(ActivityButton(self), -1)
+
+        # Create & Add Separator
+        separator = Gtk.SeparatorToolItem(draw=False)
+        separator.set_expand(True)
+        toolbar.insert(separator, -1)
+
+        # Add Stop Button
+        toolbar.insert(StopButton(self), -1)
+
+        # Create Container
+        grid = Gtk.Grid()
+
+        # Add grid to Sugar Activity GtkWindow
+        self.set_canvas(grid)
+
+        # Show all components (otherwise none will be displayed)
+        self.show_all()
 # import library here
 import pygame
 import time
